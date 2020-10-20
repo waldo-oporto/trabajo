aed3bd289eb8a34561004ffba032ccb22e2dcff5
sebastian lacatis <gachaman@outlook.it>
2020-10-20 15:39:22 +0200

    yare yare yare yare daze

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main()
+
 string Cabeza
 {
     cout << haziendo git hub perros << endl;
@@ -10,9 +11,8 @@ string Cabeza
 cout << haziendo git hub perros << endl;
 cout << nose nose <<endl;
 
-<<<<<<< HEAD
-cout << dlae lucas que podes mejorar y encontrar una waifu<<endl;
-=======
+cout << vamos lucas podes mejorar y encotrar la waifu >>
+
 
 >>>>>>> Cabeza
     return 0;
