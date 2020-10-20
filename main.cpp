40094915fe765be64efe6aaa8540b86418499f1d
sebastian lacatis <gachaman@outlook.it>
2020-10-20 15:20:14 +0200

    nueva modificacion

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main()
+string Cabeza
 {
     cout << haziendo git hub perros << endl;
 
 cout << haziendo git hub perros << endl;
+cout << nose nose <<endl;
+
+cout << dlae lucas que podes mejorar y encontrar una waifu<<endl;
     return 0;
 }
